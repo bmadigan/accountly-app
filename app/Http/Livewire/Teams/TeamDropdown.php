943be03d2f695c9a8f6a54af0c4a0554f9b75a53
Brad Madigan <bradmadigan@gmail.com>
2020-03-21 13:21:38 -0400

    WIP

---
+++ app/Http/Livewire/Teams/TeamDropdown.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace App\Http\Livewire\Teams;
+
+use Livewire\Component;
+
+class TeamDropdown extends Component
+{
+    public function render()
+    {
+        return view('livewire.teams.team-dropdown');
+    }
+}
--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -3,11 +3,20 @@
 @section('page_title') Dashboard @endsection
 
 @section('content')
+
+
     <header class="bg-white shadow-sm">
         <div class="max-w-7xl mx-auto py-4 px-4 sm:px-6 lg:px-8">
-            <h2 class="text-lg leading-6 font-semibold text-gray-900">
-                Dashboard
-            </h2>
+            <div class="md:flex md:items-center md:justify-between">
+                <div class="flex-1 min-w-0">
+                    <h2 class="flex-1 text-lg leading-6 font-semibold text-gray-900">
+                        Dashboard
+                    </h2>
+                </div>
+            <div class="mt-4 flex md:mt-0 md:ml-4">
+                @livewire('teams.team-dropdown')
+            </div>
+            </div>
         </div>
     </header>
 
