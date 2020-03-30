0f6c4b0de425d48d759ae8dfcf0227d2406b27c5
Brad Madigan <bradmadigan@gmail.com>
2020-03-30 11:35:54 -0400

    Add google login btn

--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -94,16 +94,12 @@
 
                 <div class="mt-6">
                     <div>
-                        <span class="w-full inline-flex rounded-md shadow-sm">
-                            <button type="button"
-                                class="w-full inline-flex justify-center py-2 px-4 border border-gray-300 rounded-md bg-white text-sm leading-5 font-medium text-gray-500 hover:text-gray-400 focus:outline-none focus:border-blue-300 focus:shadow-outline-blue transition duration-150 ease-in-out">
-                                <svg class="h-5 h-5" fill="currentColor" viewBox="0 0 20 20">
-                                    <path fill-rule="evenodd"
-                                        d="M20 10c0-5.523-4.477-10-10-10S0 4.477 0 10c0 4.991 3.657 9.128 8.438 9.878v-6.987h-2.54V10h2.54V7.797c0-2.506 1.492-3.89 3.777-3.89 1.094 0 2.238.195 2.238.195v2.46h-1.26c-1.243 0-1.63.771-1.63 1.562V10h2.773l-.443 2.89h-2.33v6.988C16.343 19.128 20 14.991 20 10z"
-                                        clip-rule="evenodd" />
-                                </svg>
-                            </button>
-                        </span>
+                        <div class="btn-wrapper text-center">
+                            <button
+                                class="bg-white active:bg-gray-100 text-gray-800 font-normal px-4 py-2 rounded outline-none focus:outline-none mr-1 mb-1 uppercase shadow hover:shadow-md inline-flex items-center font-bold text-xs"
+                                type="button" style="transition: all 0.15s ease 0s;"><img alt="..." class="w-5 mr-1"
+                                    src="https://demos.creative-tim.com/tailwindcss-starter-project/static/media/google.87be59a1.svg">Google</button>
+                        </div>
                     </div>
                 </div>
             </div>
