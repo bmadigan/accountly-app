86cef35d3215c191fb9e495d8dea1cd0971820eb
Brad Madigan <bradmadigan@gmail.com>
2020-08-31 12:56:53 -0400

    add new icon on team list

--- resources/views/livewire/teams/team-dropdown.blade.php
+++ resources/views/livewire/teams/team-dropdown.blade.php
@@ -35,8 +35,13 @@
                             <a class="text-sm leading-5 text-gray-700">{{ $team->name }}</a>
                         </div>
                     @else
-                        <a wire:click="switchTeam('{{ $team->id }}')"
-                           class="block px-4 py-2 text-sm leading-5 cursor-pointer text-gray-700 hover:bg-gray-100 hover:text-gray-900 focus:outline-none focus:bg-gray-100 focus:text-gray-900">{{ $team->name }}</a>
+                        <div
+                            class="flex items-center cursor-pointer block px-4 py-2 hover:bg-gray-100 hover:text-gray-900 focus:outline-none focus:bg-gray-100 focus:text-gray-900">
+                            <span class="mr-2 text-gray-400">
+                                <svg viewBox="0 0 20 20" fill="currentColor" class="user-group w-6 h-6"><path d="M13 6a3 3 0 11-6 0 3 3 0 016 0zM18 8a2 2 0 11-4 0 2 2 0 014 0zM14 15a4 4 0 00-8 0v3h8v-3zM6 8a2 2 0 11-4 0 2 2 0 014 0zM16 18v-3a5.972 5.972 0 00-.75-2.906A3.005 3.005 0 0119 15v3h-3zM4.75 12.094A5.973 5.973 0 004 15v3H1v-3a3 3 0 013.75-2.906z"></path></svg>
+                            </span>
+                            <a wire:click="switchTeam('{{ $team->id }}')" class="text-sm leading-5 text-gray-700">{{ $team->name }}</a>
+                        </div>
                     @endif
                 @endforeach
             </div>
